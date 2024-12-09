4010104be0b0c62c6c11c0b9e2b623f23d6208ca
unh6unh6 <ruddbs3308@naver.com>
2024-12-09 23:40:31 +0900

    feat(InputView): 당첨 번호 입력 안내 문구를 출력한다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_INPUT_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_INPUT_GUIDE_MESSAGE = "\n당첨 번호를 입력해 주세요.";
 
     public void printPurchaseAmountInputGuideMessage() {
         System.out.println(PURCHASE_AMOUNT_INPUT_GUIDE_MESSAGE);
@@ -11,5 +12,9 @@ public class InputView {
 
     public String getPurchaseAmount() {
         return Console.readLine();
+    }
+
+    public void printWinningNumbersInputGuideMessage() {
+        System.out.println(WINNING_NUMBERS_INPUT_GUIDE_MESSAGE);
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,7 +5,7 @@ import java.util.List;
 import lotto.dto.LottoDto;
 
 public class OutputView {
-    private static final String PURCHASE_LOTTO_MESSAGE_FORM = "%d개를 구매했습니다.\n";
+    private static final String PURCHASE_LOTTO_MESSAGE_FORM = "\n%d개를 구매했습니다.\n";
 
     public void printPurchaseLottoMessage(int purchaseCount) {
         System.out.printf(PURCHASE_LOTTO_MESSAGE_FORM, purchaseCount);
