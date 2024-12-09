cc94c2271e747cd189e0452caca82c6ee9259f7d
unh6unh6 <ruddbs3308@naver.com>
2024-12-09 23:01:29 +0900

    feat(PurchaseAmount): 구입금액이 1,000원으로 나누어 떨어지지 않는 경우 예외를 발생시킨다

--- src/main/java/lotto/ExceptionMessage.java
+++ src/main/java/lotto/ExceptionMessage.java
@@ -1,7 +1,8 @@
 package lotto;
 
 public enum ExceptionMessage {
-    PURCHASE_AMOUNT_NOT_NUMBER_EXCEPTION("구입금액은 숫자타입이어야 합니다.");
+    PURCHASE_AMOUNT_NOT_NUMBER_EXCEPTION("구입금액은 숫자타입이어야 합니다."),
+    PURCHASE_AMOUNT_INVALID_EXCEPTION("구입금액은 1,000원으로 나누어 떨어져야 합니다.");
 
 
     private final String prefix = "[ERROR] ";
---
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -0,0 +1,22 @@
+package lotto.model;
+
+import static lotto.ExceptionMessage.PURCHASE_AMOUNT_INVALID_EXCEPTION;
+
+public class PurchaseAmount {
+
+    private static final int PURCHASE_AMOUNT_UNIT = 1_000;
+
+    private final long purchaseAmount;
+
+    public PurchaseAmount(long purchaseAmount) {
+        validatePurchaseAmount(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    private void validatePurchaseAmount(long purchaseAmount) {
+        if(purchaseAmount % PURCHASE_AMOUNT_UNIT != 0) {
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_INVALID_EXCEPTION.message);
+        }
+    }
+
+}
